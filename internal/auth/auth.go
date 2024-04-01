833deefe6003678715725717876b19a01849aa34
bk167465 <bk167465@gmail.com>
2024-04-01 17:25:35 +0530

    Added auth file

---
+++ internal/auth/auth.go
@@ -0,0 +1,26 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+// GetApiKey extracts an API key from the headers of an HTTP request
+// Example:
+// Authorization: ApiKey {insert apikey here}
+func GetApiKey(headers http.Header) (string, error) {
+    val := headers.Get("Authorization")
+	if val == "" {
+		return "", errors.New("no authentcation info found")
+	}
+
+	vals := strings.Split(val, " ")
+	if len(vals) != 2 {
+		return "", errors.New("malfomed auth header")
+	}
+	if vals[0] != "ApiKey" {
+		return "", errors.New("malfomed first part of auth header")
+	}
+	return vals[1], nil
+}
